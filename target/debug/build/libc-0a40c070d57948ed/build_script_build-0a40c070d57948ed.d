dc1b2d3916ace9db097c00b133f6a0597fdba6e2
Daniel Puerta Bernal <daniel.puerta0522@uco.net.co>
2025-08-20 18:39:20 -0500

    Se agrega las dependencias necesarias para trabajar con las interfaz grefica de macroquad, y se gera una pequeña vista de prueba

---
+++ target/debug/build/libc-0a40c070d57948ed/build_script_build-0a40c070d57948ed.d
@@ -0,0 +1,5 @@
+/home/DanielP/Escritorio/SistemaDinamicos/Mundo-ValleAburra/target/debug/build/libc-0a40c070d57948ed/build_script_build-0a40c070d57948ed.d: /home/DanielP/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.175/build.rs
+
+/home/DanielP/Escritorio/SistemaDinamicos/Mundo-ValleAburra/target/debug/build/libc-0a40c070d57948ed/build_script_build-0a40c070d57948ed: /home/DanielP/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.175/build.rs
+
+/home/DanielP/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.175/build.rs:
---
+++ target/debug/deps/bitflags-473b852bda163ccf.d
@@ -0,0 +1,7 @@
+/home/DanielP/Escritorio/SistemaDinamicos/Mundo-ValleAburra/target/debug/deps/bitflags-473b852bda163ccf.d: /home/DanielP/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bitflags-1.3.2/src/lib.rs
+
+/home/DanielP/Escritorio/SistemaDinamicos/Mundo-ValleAburra/target/debug/deps/libbitflags-473b852bda163ccf.rlib: /home/DanielP/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bitflags-1.3.2/src/lib.rs
+
+/home/DanielP/Escritorio/SistemaDinamicos/Mundo-ValleAburra/target/debug/deps/libbitflags-473b852bda163ccf.rmeta: /home/DanielP/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bitflags-1.3.2/src/lib.rs
+
+/home/DanielP/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bitflags-1.3.2/src/lib.rs:
