e2e3866030dc2267b8023ae1efa567db2fb4bf1e
JDMQXD <juanda@mx>
2025-09-13 07:09:27 -0500

    otra ves

---
+++ CodigoFuente/target/debug/deps/equivalent-fb6cc83cf281e9bc.d
@@ -0,0 +1,7 @@
+/home/DanielP/Escritorio/SistemaDinamicos/Mundo-ValleAburra/target/debug/deps/equivalent-fb6cc83cf281e9bc.d: /home/DanielP/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/equivalent-1.0.2/src/lib.rs
+
+/home/DanielP/Escritorio/SistemaDinamicos/Mundo-ValleAburra/target/debug/deps/libequivalent-fb6cc83cf281e9bc.rlib: /home/DanielP/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/equivalent-1.0.2/src/lib.rs
+
+/home/DanielP/Escritorio/SistemaDinamicos/Mundo-ValleAburra/target/debug/deps/libequivalent-fb6cc83cf281e9bc.rmeta: /home/DanielP/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/equivalent-1.0.2/src/lib.rs
+
+/home/DanielP/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/equivalent-1.0.2/src/lib.rs:
---
+++ CodigoFuente/target/debug/deps/quad_rand-f3c956762ec0f488.d
@@ -0,0 +1,8 @@
+/home/DanielP/Escritorio/SistemaDinamicos/Mundo-ValleAburra/target/debug/deps/quad_rand-f3c956762ec0f488.d: /home/DanielP/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quad-rand-0.2.3/src/lib.rs /home/DanielP/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quad-rand-0.2.3/src/fy.rs
+
+/home/DanielP/Escritorio/SistemaDinamicos/Mundo-ValleAburra/target/debug/deps/libquad_rand-f3c956762ec0f488.rlib: /home/DanielP/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quad-rand-0.2.3/src/lib.rs /home/DanielP/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quad-rand-0.2.3/src/fy.rs
+
+/home/DanielP/Escritorio/SistemaDinamicos/Mundo-ValleAburra/target/debug/deps/libquad_rand-f3c956762ec0f488.rmeta: /home/DanielP/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quad-rand-0.2.3/src/lib.rs /home/DanielP/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quad-rand-0.2.3/src/fy.rs
+
+/home/DanielP/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quad-rand-0.2.3/src/lib.rs:
+/home/DanielP/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quad-rand-0.2.3/src/fy.rs:
